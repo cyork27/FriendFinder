25fd80036f1ec311dc812cf29449533778656442
cyork27 <chris.york714@gmail.com>
2018-03-03 10:02:42 -0600

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var friends = [
+{
+	name: "Jim",
+	photo: "https://img.buzzfeed.com/buzzfeed-static/static/2016-04/23/10/campaign_images/webdr02/what-percent-compatible-with-jim-halpert-from-the-2-24963-1461422235-11_dblbig.jpg",
+	scores: [
+	"5",
+	"1",
+	"4",
+	"4",
+	"5",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1"
+	]
+},
+
+{
+	name: "Jim",
+	photo: "https://img.buzzfeed.com/buzzfeed-static/static/2016-04/23/10/campaign_images/webdr02/what-percent-compatible-with-jim-halpert-from-the-2-24963-1461422235-11_dblbig.jpg",
+	scores: [
+	"5",
+	"1",
+	"4",
+	"4",
+	"5",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1"
+	]
+},
+
+{
+	name: "Jim",
+	photo: "https://img.buzzfeed.com/buzzfeed-static/static/2016-04/23/10/campaign_images/webdr02/what-percent-compatible-with-jim-halpert-from-the-2-24963-1461422235-11_dblbig.jpg",
+	scores: [
+	"5",
+	"1",
+	"4",
+	"4",
+	"5",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1"
+	]
+},
+
+];
+
+module.exports = friends;
